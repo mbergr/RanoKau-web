aa7738a65b335dad27178ceb5b226196c0ae3d07
Mathias Berg <mathias.berg.1992@gmail.com>
2022-11-04 16:52:24 -0300

    Arreglos varios
    agregar fotos, y views proyectos

--- projects/views.py
+++ projects/views.py
@@ -12,11 +12,11 @@ import geocoder
 # Create your views here.
 def projects(request):
     projects = Project.objects.all()
-    avg_projects = Project.objects.all().aggregate(Avg('complete_per'))['complete_per__avg']
+    #avg_projects = Project.objects.all().aggregate(Avg('complete_per'))['complete_per__avg']
     tasks = Task.objects.all()
     overdue_tasks = tasks.filter(due='2')
     context = {
-        'avg_projects' : avg_projects,
+        #'avg_projects' : avg_projects,
         'projects' : projects,
         'tasks' : tasks,
         'overdue_tasks' : overdue_tasks,
